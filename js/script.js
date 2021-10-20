f5fd813193f49ae686fbaf5f75d74f2132699817
Linuxpert <g.manfre10@gmail.com>
2021-10-20 16:30:39 +0200

    ultime modifiche

--- js/script.js
+++ js/script.js
@@ -1,13 +1,10 @@
 //Chiedi all'utente il suo nome
 let nome = prompt("Scrivi il tuo nome");
-console.log(nome);
 //Chiedi all'utente il suo cognome
 let cognome = prompt("Scrivi il tuo cognome");
-console.log(cognome);
 //Chiedi all'utente il suo colore preferito
 let colore = prompt("Scrivi il tuo colore preferito");
-console.log(colore);
 //compila la sua password
 const numero = 21;
-
+console.log(nome, cognome, colore);
 document.getElementById("password").innerHTML = `${"La password assegnata è: " + nome + cognome + colore + numero}`
